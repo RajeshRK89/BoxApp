047207699a0685b33e6569fb4e3a7829235cc140
TE376346 <rajesh.rk@te.com>
2020-11-29 17:52:35 +0530

    "ui changes and root folder fetch"

--- boxproj/boxapp/views.py
+++ boxproj/boxapp/views.py
@@ -1,11 +1,11 @@
 
 from django.shortcuts import render
 
-
+from boxsdk import JWTAuth, Client
 
 def index(request):
     file_list = {}
-    from boxsdk import JWTAuth, Client
+    
     
     
 
@@ -17,13 +17,29 @@ def index(request):
     files=service_account.get_items()
 
     # print(file)
+    service_account = client.folder('0')
+    folders=service_account.get_items()
 
-    for val in files:
-        tot_str=('File Type: {} - File ID: {} - File Name: {}').format(val.type,val.id,val.name)
-        
-        file_list[val.id]=tot_str
-        # print(val.id, val.name , val.type )
-    print(file_list)
+    # print(file)
+
+    for val in folders:
+        # fold_str=('Folder Type: {} - Folder Name: {}').format(val.type,val.id,val.name)
+        if val.type == 'folder':
+            # print(type(val.id))
+            get_files = client.folder(val.id)
+            files = get_files.get_items()
+
+            for file_name in files:
+
+                file_str=('File Type: {} - File Name: {} - Belongs to(Folder): {} ').format(file_name.type,file_name.name,val.name)
+                file_list['File - '+file_name.id]=file_str
+        elif val.type == 'file':
+            file_str=('File Type: {} - File Name: {} ').format(val.type,val.name)
+            file_list['File - '+val.id]=file_str
+        else:
+            pass
+
+  
     
     context={'file_list':file_list}
    
